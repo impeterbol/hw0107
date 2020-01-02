703a155139d9acbcab26eb142ded6eb5d596bac4
peterboldyrev <53663056+peterboldyrev@users.noreply.github.com>
2020-01-01 22:39:58 -0800

    added the general structure + basic code - early commit

---
+++ questions.js
@@ -0,0 +1,36 @@
+// ### Hints
+
+// * Store your questions as an array of objects in a separate file, `questions.js`, that follows this format:
+alert("connected");
+// ```js
+let questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+   title: "The condition in an if / else statement is enclosed within ____.",
+   choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+   answer: "parentheses"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+   },
+   {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+   }
+  
+];
+
+
+// * The length of the array in `questions.js` determines the length of play. Fifteen seconds per question is a good estimate, so 5 questions will result in a length of play of 75 seconds.
